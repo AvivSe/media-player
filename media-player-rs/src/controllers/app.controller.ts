48077a74d41aa6180714a877ab74ed053e57e01b
Aviv <avivsegal@gmail.com>
2019-12-08 20:58:07 +0200

    add mvc folder

--- media-player-rs/src/controllers/app.controller.ts
+++ media-player-rs/src/controllers/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from '../services/app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
