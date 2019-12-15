30087a96a2a2facd3b8e2f2c12ddbc9b9472f8f0
Aviv <avivsegal@gmail.com>
2019-12-15 15:46:58 +0200

    add creation form

--- media-player-gui/src/components/Dialog.js
+++ media-player-gui/src/components/Dialog.js
@@ -1,11 +1,7 @@
 import React from "react";
-import { Dialog as MuiDialog, IconButton, makeStyles, Slide, Toolbar } from "@material-ui/core";
+import { Dialog as MuiDialog, IconButton, Toolbar } from "@material-ui/core";
 import { FaTimesCircle as CloseIcon } from "react-icons/fa";
 import styled from "styled-components";
-
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
 
 const MainContent = styled.div`
   margin: 4rem auto;
