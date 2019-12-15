32e22218d9a0e7836cd95a2d9333cd205867621c
avivsegal <avivsegal@gmail.com>
2019-12-16 00:38:43 +0200

    add dynamic port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.PORT || 8080, '0.0.0.0');
 }
 bootstrap();
