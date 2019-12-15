dfa858aa3b75a8708a4bea82440e3cc3a4fbba5a
avivsegal <avivsegal@gmail.com>
2019-12-16 01:46:02 +0200

    fix public path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { join } from 'path';
       useUnifiedTopology: true,
     }),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..','dist', 'client'),
+      rootPath: join(__dirname, 'client'),
       renderPath: '*',
     }),
   ],
