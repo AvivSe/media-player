83743cabed9a2aeb541cc455110b074b4c6f16e5
avivsegal <avivsegal@gmail.com>
2019-12-16 23:32:23 +0200

    client build

---
+++ dist/client/precache-manifest.95df94233750f0cf53b1944e1030b67e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2773f7dcf1d1b4a24fe1fb372b391a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1b418e9e798b7191d8b",
+    "url": "/static/css/2.73dad30a.chunk.css"
+  },
+  {
+    "revision": "985e5b402a75ded19a66",
+    "url": "/static/css/main.789974f3.chunk.css"
+  },
+  {
+    "revision": "f1b418e9e798b7191d8b",
+    "url": "/static/js/2.fa1e1a58.chunk.js"
+  },
+  {
+    "revision": "88406d08ebe2d2b13ee6f4a8e962365a",
+    "url": "/static/js/2.fa1e1a58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "985e5b402a75ded19a66",
+    "url": "/static/js/main.b6ff9f92.chunk.js"
+  },
+  {
+    "revision": "4ab3f666e4ab5d709826",
+    "url": "/static/js/runtime-main.c7b625d4.js"
+  },
+  {
+    "revision": "e0498a5669e7b4b82ef96d9b750f3334",
+    "url": "/static/media/hellyeah.e0498a56.svg"
+  }
+]);
